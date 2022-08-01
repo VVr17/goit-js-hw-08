c3d5697b29d21090b63a015f44154b4d654787af
Vira Voronova <vera.voronova@hotmail.com>
2022-08-01 14:40:22 +0200

    second task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay,1000));
+
+function onPlay(event) {
+    localStorage.setItem("videoplayer-current-time", `${event.seconds}`);
+}
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+        default:
+            // some other error occurred
+            break;
+    }
+});
